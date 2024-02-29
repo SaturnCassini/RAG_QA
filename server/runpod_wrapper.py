df7764e52422d6fe819dd420a0412113075b57ee
Pablo Perez <pablo.sky@hotmail.com>
2024-02-29 15:43:09 +0100

    ADD: server copy from pooya

---
+++ server/runpod_wrapper.py
@@ -0,0 +1,42 @@
+import runpod
+from typing import Any, Literal, TypedDict
+import requests
+import sys
+
+
+class HandlerInput(TypedDict):
+    """The data for calling the Ollama service."""
+
+    method_name: Literal["generate"]
+    """The url endpoint of the Ollama service to make a post request to."""
+
+    input: Any
+    """The body of the post request to the Ollama service."""
+
+
+class HandlerJob(TypedDict):
+    input: HandlerInput
+
+
+def handler(job: HandlerJob):
+    base_url = "http://0.0.0.0:11434"
+    input = job["input"]
+
+    # streaming is not supported in serverless mode
+    input["input"]["stream"] = False
+    print(sys.argv)
+    model = sys.argv[1]
+    input["input"]["model"] = model
+
+    response = requests.post(
+        url=f"{base_url}/api/{input['method_name']}/",
+        headers={"Content-Type": "application/json"},
+        json=input["input"],
+    )
+    response.encoding = "utf-8"
+
+    # TODO: handle errors
+    return response.json()
+
+
+runpod.serverless.start({"handler": handler})
